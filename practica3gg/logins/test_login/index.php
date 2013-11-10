281099b02c8c480b700583326533956244fd46cb
ggoral <goral.gonzalo@gmail.com>
2013-11-10 19:20:12 -0300

    upgrade fix test_login

--- practica3gg/logins/test_login/index.php
+++ practica3gg/logins/test_login/index.php
@@ -1,4 +1,18 @@
 <?php
-  header('Location: index.html');
-  //poner una direccion absoluto
+session_start();
+
+if (array_key_exists('usuario', $_SESSION)) {
+  if (isset($_SERVER))
+    {
+    $HOST = $_SERVER['HTTP_HOST'];
+    $URI = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $EXTRA = 'index.html';
+    header("Location: http://$HOST$URI/$EXTRA");
+    require('vista.php');
+    }
+  }
+else {
+    header("Location: index.html");
+  }
+exit;
 ?>

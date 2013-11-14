5cf6c0cd5482018cba311bc85a217356ea97d5b2
ggoral <goral.gonzalo@gmail.com>
2013-11-14 15:04:49 -0300

    after this, there is no turning back

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#scp -r $PWD/practica3gg/logins/test_login root@172.16.207.133:/var/www/
-scp -r $PWD/practica3gg/ root@172.16.207.133:/var/www/proyecto2013/
+scp -r $PWD/practica3gg/logins/test_login root@172.16.207.133:/var/www/
+#scp -r $PWD/practica* root@172.16.207.133:/var/www/proyecto2013/

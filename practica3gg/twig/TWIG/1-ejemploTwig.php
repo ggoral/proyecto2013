f487713bb8f82b1282a11b954d95b4b3e528de2c
ggoral <goral.gonzalo@gmail.com>
2013-11-13 21:43:19 -0300

    begin twig examples

--- practica3gg/twig/TWIG/1-ejemploTwig.php
+++ practica3gg/twig/TWIG/1-ejemploTwig.php
@@ -3,21 +3,16 @@ require_once("Twig/Autoloader.php");
 Twig_Autoloader::register();
 
 $templateDir="./templates";
-$templateDirCompi="./templates-c";
 
 $loader = new Twig_Loader_Filesystem($templateDir);
 
 $twig = new Twig_Environment($loader);
-//, array("cache" => $templateDirCompi,
-//           ));
-            
+
 $template = $twig->loadTemplate("plantilla-1.twig");
-//Probemos descomentando el de abajo y comentadno el de arriba
-//$template = $twig->loadTemplate("pruebas.html");
 
-
-$template->display(array('nombre' => 'Claudia!',
-						'lista' => array("1","2",223,4,5, 6)
+$template->display(array('nombre' => 'Gonzalo!',
+						             'lista' => array("1","2",3,3,1983, 6,"Gonzalo"),
+                         'footer' => 'El pie de pagina',
 ));
 
 ?>
